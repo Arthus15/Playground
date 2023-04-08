19e157adbcbb2064de8a6d05f94848ddeda3695a
Arthus15 <JairoBlanco15@gmail.com>
2023-04-08 19:08:51 +0200

    First commit

---
+++ Playground/Games/GameOfLife/GameOfLife.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Playground.Games.GameOfLife
+{
+	public partial class GameOfLife: ComponentBase
+	{
+		public int CellCount = 400;
+		private int[] _cellStatus;
+
+		public override Task SetParametersAsync(ParameterView parameters)
+		{
+			_cellStatus = Enumerable.Repeat(0, CellCount).ToArray();
+
+			return base.SetParametersAsync(parameters);
+		}
+
+		void UpdateCell(int index)
+		{
+			_cellStatus[index] = _cellStatus[index] is 0 ? 1 : 0;
+			Console.WriteLine($"Cell[{index}] = {_cellStatus[index]}");
+			StateHasChanged();
+		}
+
+		string SetStyle(int index)
+		{
+			if (_cellStatus[index] is 0)
+			{
+				return "background: #ffffff";
+			}
+
+			return "background: #1CFF8A";
+		}
+	}
+}
